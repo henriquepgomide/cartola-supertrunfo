059c6604c764df7b73f5d178a923ed9cdbdab64c
Henrique Gomide <henriquepgomide@gmail.com>
2020-05-11 21:51:40 -0300

    deploy

---
+++ precache-manifest.12612e40043114b35372901d1568fa62.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e20b01dc95b7db44ab8",
+    "url": "/cartola-supertrunfo/css/app.834c4de1.css"
+  },
+  {
+    "revision": "cab355d8510df780c225",
+    "url": "/cartola-supertrunfo/css/chunk-vendors.a9e64ce6.css"
+  },
+  {
+    "revision": "541b565c42a6e31e3a2f078476a50ca6",
+    "url": "/cartola-supertrunfo/img/back_card.jpeg"
+  },
+  {
+    "revision": "7412844ec4525d19a6c860645678b853",
+    "url": "/cartola-supertrunfo/img/pfc-logo.7412844e.png"
+  },
+  {
+    "revision": "c08397c32d80a271bde21e56e7763c8e",
+    "url": "/cartola-supertrunfo/index.html"
+  },
+  {
+    "revision": "5e20b01dc95b7db44ab8",
+    "url": "/cartola-supertrunfo/js/app.37271343.js"
+  },
+  {
+    "revision": "cab355d8510df780c225",
+    "url": "/cartola-supertrunfo/js/chunk-vendors.21f72a2f.js"
+  },
+  {
+    "revision": "cd6e9a336216616c1861e9b5ae281b00",
+    "url": "/cartola-supertrunfo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cartola-supertrunfo/robots.txt"
+  }
+]);
